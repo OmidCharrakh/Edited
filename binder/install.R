0e977af02abfb9cdf5fb562179856266664e64df
OmidCharrakh <omid.charrakh@gmail.com>
2020-07-25 14:44:19 +0200

    uninstall unnecessary packages

--- binder/install.R
+++ binder/install.R
@@ -1,7 +1,3 @@
 install.packages("tidyverse")
 install.packages("rjson")
 install.packages("mlr3")
-install.packages("randomForest")
-install.packages("rpart")
-install.packages("ranger")
-install.packages("tidymodels")
